c3fa0b1e79e7dd7d0a4d09306a750fcdbd95d546
Florian Serraille <florianserraille@gmail.com>
2020-12-16 17:54:55 -0300

    [inventory-service] Adding flywaydb migration

---
+++ inventory-service/src/main/resources/db/import.sql
@@ -0,0 +1,4 @@
+-- executed on startup if Hibernate creates the schema from scratch (that is, if the ddl-auto property is set to create or create-drop).
+
+insert into PRODUCT values (1, 'First product', 10.0);
+insert into PRODUCT values (2, 'Second product', 20.5);
