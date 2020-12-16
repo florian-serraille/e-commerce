9a44993ae7bcaf86c2d9952e7601df9313aba73d
Florian Serraille <florianserraille@gmail.com>
2020-12-16 19:01:37 -0300

    [inventory-service] Improve import.sql script

--- inventory-service/src/main/resources/db/import.sql
+++ inventory-service/src/main/resources/db/import.sql
@@ -1,17 +1,9 @@
--- executed on startup if Hibernate creates the schema from scratch (that is, if the ddl-auto property is set to create or create-drop).
+insert into PRODUCT
+values ((SELECT nextval('PRODUCT_SEQ')),
+        'A product',
+        10.0);
 
 insert into PRODUCT
-values (1, 'First product', 10.0);
-SELECT nextval('PRODUCT_SEQ');
-
-insert into PRODUCT
-values (2, 'Second product', 20.5);
-SELECT nextval('PRODUCT_SEQ');
-
-insert into PRODUCT
-select 1,
-       'First product',
-       10.0
-where not exists(
-        select 1 from PRODUCT where id = 1
-    );
+values ((SELECT nextval('PRODUCT_SEQ')),
+        'Another product',
+        20.5);
