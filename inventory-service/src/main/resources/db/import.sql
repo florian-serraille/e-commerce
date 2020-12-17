a4d830300dece449a8ecc6ea3f29aeb32738b01e
Florian Serraille <florianserraille@gmail.com>
2020-12-17 18:55:49 -0300

    [inventory-service] Adding fk's constraints into product table

--- inventory-service/src/main/resources/db/import.sql
+++ inventory-service/src/main/resources/db/import.sql
@@ -1,13 +1,19 @@
+insert into BRAND (ID, NAME)
+values ((SELECT nextval('BRAND_SEQ')), 'A brand');
+
+insert into CATEGORY (ID, NAME)
+values ((SELECT nextval('CATEGORY_SEQ')), 'A category');
+
 insert into PRODUCT (ID, NAME, UNIT_PRICE, BRAND_ID, CATEGORY_ID)
 values ((SELECT nextval('PRODUCT_SEQ')),
         'A product',
+        10.0,
         1,
-        1,
-        10.0);
+        1);
 
 insert into PRODUCT (ID, NAME, UNIT_PRICE, BRAND_ID, CATEGORY_ID)
 values ((SELECT nextval('PRODUCT_SEQ')),
         'Another product',
+        20.5,
         1,
-        1,
-        20.5);
+        1);
