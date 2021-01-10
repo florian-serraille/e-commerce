0af2aba3187d59734e9c781445ff857e98bbf892
Florian Serraille <florianserraille@gmail.com>
2021-01-10 05:44:55 -0300

    [configuration-service] Implement configuration service

---
+++ configuration-service/src/main/resources/db/migration/V2__comon-properties.sql
@@ -0,0 +1,6 @@
+INSERT INTO PROPERTIES VALUES (1, 'application', 'default', 'master', 'api.documentation.contact.name', 'Florian Serraille');
+INSERT INTO PROPERTIES VALUES (2, 'application', 'default', 'master', 'api.documentation.contact.email', 'florianserraille@gmail.com');
+INSERT INTO PROPERTIES VALUES (3, 'application', 'default', 'master', 'api.documentation.contact.url', 'https://github.com/florian-serraille');
+
+INSERT INTO PROPERTIES VALUES (4, 'application', 'default', 'master', 'spring.jpa.hibernate.ddl-auto', 'validate');
+INSERT INTO PROPERTIES VALUES (5, 'application', 'default', 'master', 'spring.flyway.base-line-on-migrate', 'true');
